f59290b3a30bf30ac3a0ed45a91733df622296bf
Mihai Osvat <mihai.osvat@nordlogic.com>
2019-01-02 09:14:48 +0200

    Fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { Module } from '@nestjs/common';
 import * as bonjour from 'bonjour';
+
 import { DiscoveredServices } from '../app/config';
 import { RoutingModule } from './routes/routing.module';
+import { RabbitMessageQueue } from './shared/mq/mq.service';
+import { LoggingService } from './shared/logging/logging.service';
 
 @Module({
-    imports: [RoutingModule],
+    imports: [ RoutingModule ],
 })
 export class AppModule {
+    constructor( private mqService: RabbitMessageQueue, private loggingService: LoggingService ) {  }
     async onModuleInit() {
+        await this.mqService.initializeConnection();
+        await this.mqService.ensureInfrastructure();
+        await this.loggingService.getLogger().info(`Starting API Gateway`);
+
         const browser = bonjour();
-        browser.find({
-            type: 'rue-service',
-        }, (service) => {
+
+        browser.find({ type: 'rue-service' }, (service) => {
             DiscoveredServices.push(service.name);
         });
     }
